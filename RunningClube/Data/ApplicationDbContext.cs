855f943020d7ab4d01cd29d6f48bee061c2e0e21
syakil <syakilahmadginanjar@gmail.com>
2025-06-19 04:38:21 +0700

    model

---
+++ RunningClube/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningClube.Models;
+
+namespace RunningClube.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ RunningClube/Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunningClube.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
