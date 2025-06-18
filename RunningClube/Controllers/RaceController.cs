89d08e8eeeea74627fa7ee47821f1761b3ae68bd
syakil <syakilahmadginanjar@gmail.com>
2025-06-19 04:38:13 +0700

    controller

---
+++ RunningClube/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunningClube.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
