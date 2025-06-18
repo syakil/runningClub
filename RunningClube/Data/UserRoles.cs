6584b5f3a8fbd9e1dde93d6f43587e4109528c15
syakil <syakilahmadginanjar@gmail.com>
2025-06-19 04:37:51 +0700

    Update .gitignore to exclude temp files

--- RunningClube/Data/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RunningClube.Data
-{
-    public static class UserRoles
-    {
-        public const string Admin = "admin";
-        public const string User = "user";
-    }
-}
--- RunningClube/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RunningClube.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
